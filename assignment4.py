dd274ce5a7f7389fa5611518bf23f22f778a58bb
HARAJIT DAS <110291590+HARAJIT05@users.noreply.github.com>
2023-03-16 10:40:58 +0530

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -1,91 +1,42 @@
-#############################
-#IMPLEMENT OF STACK IN PYTHON
-###############################
-import numpy as np
-stack=np.zeros(5,dtype='int16')
-top=-1
-def push(n):
-    global top
-    if top==4:
-        print("overflow")
-        return
-    else:
-        top=top+1
-        stack[top]=n
-def pop():
-    global top
-    if top==-1:
-        print("underflow")
-    else:
-        print("deleted element is",stack[top])
-        top=top-1
-while True:
-    ch=int(input("<1>insert<2>delete<3>exit"))
-    if (ch==1):
-        x=int(input("enter a value"))
-        push(x)
-    elif (ch==2):
-        pop()
-    elif (ch==3):
-        break
-#################
-#OMPLEMENT OF QUEUE IN PYTHON
-#################
-import numpy as np
-n=np.zeros(10,dtype=int)
-rear=-1
-front=0
-def insert(n):
-    global rear
-    if rear==9:
-        print("OVERFLOW")
-    else:
-        rear+=1
-        n[rear]=int(input("ENTER THE ELEMENT : "))
-def delete():
-    global front
-    if front>rear:
-        print("UNDERFLOW")
-    else:
-        print("DELETED ELEMENT IS:",n[front])
-        front=front+1
-while True:
-    ch =int(input("<1> INSERT <2> DELETE <3> EXIT: "))
-    if ch==1:
-        insert(n)
-    elif ch==2:
-        delete()
-    elif ch==3:
-        break
- ########################
-#IMPLEMENT OF CIRCULER QUEUE
-#############################
-import numpy as np
-front=0
-rear=-1
-queue=np.zeros(10,dtype='int16')
-def insert(n):
-    global rear
-    if rear-front==9:
-        print("overflow!!")
-    else:
-        rear=rear+1
-        queue[rear%10]=n
-        print("inserted element is:",queue[rear%10])
-def pop():
-    global front
-    if front>rear:
-        print("Underflow!")
-    else:
-        print("deleted element is:",queue[front%10])
-        top=top+1
-while True:
-    ch=int(input("<1 push><2 pop><3 exit>"))
-    if ch==1:
-        x=int(input("enter an element: "))
-        insert(x)
-    elif ch==2:
-        pop()
-    elif ch==3:
-        break
-
+class List:
+    def __init__(self, n):
+        self.data = n
+        self.address = None
+class LinkList:
+    def __init__(self):
+        self.head = None
+        self.last = None
+    def createlist(self, n):
+        newnode = List(n)
+        if self.head is None:
+            self.head = newnode
+        else:
+            self.last.address = newnode
+        self.last = newnode
+    def printlist(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.data)
+            temp = temp.address
+    def countnumbers(self):
+        temp = self.head
+        count = 0
+        while temp is not None:
+            count += 1
+            temp = temp.address
+        print(count)
+    def max(self):
+        temp=self.head
+        max=temp.data
+        while temp is not None:
+            if temp.data>max:
+                max=temp.data
+            temp=temp.address
+        print(max)
+s = LinkList()
+s.createlist(10)
+s.createlist(20)
+s.createlist(30)
+s.countnumbers()
+s.max()
+s.printlist()
